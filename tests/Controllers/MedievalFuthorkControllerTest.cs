308ef383da5f0aa1ea56de19af9ae2b1e3194f35
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 12:59:00 +0300

    Add Medieval Futhork controller/endpoint

---
+++ tests/Controllers/MedievalFuthorkControllerTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using RunesAPI.Controllers;
+using RunesAPI.Services;
+
+namespace RunesAPITests.Controllers
+{
+    public class MedievalFuthorkControllerTest
+    {
+        [Fact]
+        public void Endpoint_Returns_Given_Content_In_Elder_Futhark_Runes()
+        {
+            var controller = new MedievalFuthorkController(new RunesService());
+
+            string content = "lorem ipsum";
+            string expected = "ᛚᚮᚱᚽᛘ:ᛁᛕᛋᚢᛘ";
+            var result = controller.Get(content);
+            var value = (OkObjectResult)result.Result;
+
+            Assert.Equal(expected, result.Value);
+        }
+    }
+}
