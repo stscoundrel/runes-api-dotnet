b85f3c21caaa848e6c36a045bd37714eaf86491e
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 12:51:56 +0300

    Integration tests: refactor status/contentype to be same for all endpoints

--- tests/IntegrationTests.cs
+++ tests/IntegrationTests.cs
@@ -17,7 +17,8 @@ namespace RunesAPITests
 
         [Theory]
         [InlineData("/api/younger-futhark/lorem-ipsum")]
-        public async Task Younger_Futhark_Endpoint_Returns_Success_And_Correct_Content_Type(string url)
+        [InlineData("/api/elder-futhark/lorem-ipsum")]
+        public async Task Rune_Endpoints_Return_Success_And_Correct_Content_Type(string url)
         {
             var client = _factory.CreateClient();
             var response = await client.GetAsync(url);
@@ -28,9 +29,29 @@ namespace RunesAPITests
             // Plain text
             Assert.Equal("text/plain; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Younger_Futhark_Endpoint_Returns_Correct_Content()
+        {
+            var url = "/api/younger-futhark/lorem-ipsum";
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
 
             // Expected body content.
             Assert.Equal("ᛚᚢᚱᛁᛘ-ᛁᛒᛋᚢᛘ",
+                response.Content.ReadAsStringAsync().Result);
+        }
+
+        [Fact]
+        public async Task Elder_Futhark_Endpoint_Returns_Correct_Content()
+        {
+            var url = "/api/elder-futhark/lorem-ipsum";
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+
+            // Expected body content.
+            Assert.Equal("ᛚᛟᚱᛖᛗ-ᛁᛈᛋᚢᛗ",
                 response.Content.ReadAsStringAsync().Result);
         }
     }
