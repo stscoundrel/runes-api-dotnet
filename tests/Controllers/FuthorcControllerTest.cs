6fcab7d0feb0cd8296e428b3deebde4120fb1717
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 13:08:47 +0300

    Add Futhorc controller/endpoint

---
+++ tests/Controllers/FuthorcControllerTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using RunesAPI.Controllers;
+using RunesAPI.Services;
+
+namespace RunesAPITests.Controllers
+{
+    public class FuthorcControllerTest
+    {
+        [Fact]
+        public void Endpoint_Returns_Given_Content_In_Elder_Futhark_Runes()
+        {
+            var controller = new FuthorcController(new RunesService());
+
+            string content = "lorem ipsum";
+            string expected = "ᛚᚩᚱᛖᛗ:ᛁᛈᛋᚢᛗ";
+            var result = controller.Get(content);
+            var value = (OkObjectResult)result.Result;
+
+            Assert.Equal(expected, result.Value);
+        }
+    }
+}
